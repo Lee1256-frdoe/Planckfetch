fcb6aba8030591c2d87e12b79c8170bdc6da4fe8
Lee1256-frdoe <kaileon8789@gmail.com>
2024-09-30 00:48:56 +0200

    Rename microfetch.sh to planckfetch.sh

--- microfetch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-source ./logo.sh && dist=$(lsb_release -ds) && TIME="$(date +"%T")" && CPU="$(grep -m 1 'model name' /proc/cpuinfo)" && CORES="$(grep -c 'model name' /proc/cpuinfo)" && Arch="$(uname --machine)" && GPU="$(glxinfo | grep -Ei 'device|memory' | grep Device)" && RAM="$(grep MemTotal /proc/meminfo)" && RAMFREE="$(grep MemFree /proc/meminfo)" && echo -e '\t\033[0;37m'USER \& HOST: $USER@$HOSTNAME '\n\t'TIME: $TIME '\n\t'DISTRO: $dist '\n\t'CPU $CPU';' Cores: $CORES '\n\t'CPU Architecture: $Arch '\n\t'GPU $GPU '\n\t'RAM $RAM '\n\t'RAM $RAMFREE
